79283f73d2c6c35b5e2493c9b8a0b7bd01ed0877
humzarasheed <hamzarasheed679@gmail.com>
2025-03-18 03:39:43 +0500

    added projects

--- src/app/assets/assets.js
+++ src/app/assets/assets.js
@@ -5,10 +5,27 @@ import gaming from "./gaming.PNG";
 import oams from "./oams.PNG";
 import doctors from "./doctors_appointment_system.PNG";
 import hero from "./hero.png";
-
-
-
-
+import multiDental from "./mds.PNG";
+import Universal_Real_Estate from "./universal_real_estate.PNG";
+import aspca from "./aspca.png";
+import jadidkhata from "./jadidkhata.PNG";
+import ahc from "./ahc.PNG";
+import BurgerStation from "./burger_station.PNG";
+import ska from "./ska.PNG";
+import solarinsure from "./solarinsure.PNG";
+import hybridemployment from "./hybridemployment.PNG";
+import katchery from "./katchery.PNG";
+import sabziexpress from "./sabziexpress.PNG";
+import seekooh from "./seekooh.PNG";
+import aaptive from "./aaptiv.PNG";
+import carGlow from "./carglow.PNG";
+import AamClick from "./aamclick.PNG";
+import BlackAcademy from "./theblackacademy.png";
+import desboites from "./desboites.PNG";
+import rpl from "./rpl.png";
+import firstClass from "./1stclass.png";
+import monarchu from "./monarchu.png";
+import bigwig from "./bigwig.png";
 
 export const assets = {
     srms,
@@ -18,4 +35,25 @@ export const assets = {
     oams,
     doctors,
     hero,
+    multiDental,
+    Universal_Real_Estate,
+    aspca,
+    jadidkhata,
+    ahc,
+    BurgerStation,
+    ska,
+    solarinsure,
+    hybridemployment,
+    katchery,
+    sabziexpress,
+    seekooh,
+    aaptive,
+    carGlow,
+    AamClick,
+    BlackAcademy,
+    desboites,
+    rpl,
+    firstClass,
+    monarchu,
+    bigwig
 }
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,6 +8,78 @@ import { assets } from "../assets/assets";
 
 const projectsData = [
   {
+    id: 24,
+    title: "Reliable Personal Loans",
+    description: "Online Loan application portal",
+    image: assets.rpl.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://reliablepersonalloans.com/",
+  },
+  {
+    id: 14,
+    title: "Solar Insure",
+    description: "Solar Products Warranty Provider",
+    image: assets.solarinsure.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://www.solarinsure.com/",
+  },
+  {
+    id: 15,
+    title: "Hybrid Employment",
+    description: "Remote work System",
+    image: assets.hybridemployment.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://hybridemployment.com/",
+  },
+  {
+    id: 25,
+    title: "1st Class Platform",
+    description: "Online Real Estate platform",
+    image: assets.firstClass.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://1stclassplatform.com/",
+  },
+  {
+    id: 26,
+    title: "Monarch Environmental",
+    description: "Platform that offer water management, contract management services.",
+    image: assets.monarchu.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "http://monarchu.com/",
+  },
+  {
+    id: 27,
+    title: "BigWig ERP",
+    description: "Online ERP system with CRM and POS.",
+    image: assets.bigwig.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://bigwigerp.com/",
+  },
+  {
+    id: 16,
+    title: "Katchery",
+    description: "File court casees online",
+    image: assets.katchery.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://katchery.com/",
+  },
+  {
+    id: 18,
+    title: "Seekooh",
+    description: "Online learning platform",
+    image: assets.seekooh.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "https://seekooh.com/",
+  },
+  {
     id: 1,
     title: "Student Record Management System",
     description: "University record management system. Students can view their results online.",
@@ -37,7 +109,7 @@ const projectsData = [
   {
     id: 4,
     title: "Online Gaming Website",
-    description: "'Build for any organization/school/university. Users can schedule different games and share the link with others",
+    description: "Build for any organization/school/university. Users can schedule different games and share the link with others",
     image: assets.gaming.src,
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -60,6 +132,123 @@ const projectsData = [
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "Online Dental Store",
+    description: "An online ecommerece store related to buying and selling medical and dental products",
+    image: assets.multiDental.src,
+    tag: ["All", "PHP"],
+    gitUrl: "/",
+    previewUrl: "http://multidental.pk/",
+  },
+  {
+    id: 8,
+    title: "Universal Real Estate",
+    description: "Online property management system",
+    image: assets.Universal_Real_Estate.src,
+    tag: ["All", "PHP"],
+    gitUrl: "/",
+    previewUrl: "https://universal-realestates.com/",
+  },
+  {
+    id: 9,
+    title: "Doobert ASPCA",
+    description: "American Society for the Prevention of Cruelty to Animals",
+    image: assets.aspca.src,
+    tag: ["All", "Laraval"],
+    gitUrl: "/",
+    previewUrl: "https://www.aspca.org/",
+  },
+  {
+    id: 10,
+    title: "Jadid Khata",
+    description: "Online POS System",
+    image: assets.jadidkhata.src,
+    tag: ["All", "Laraval"],
+    gitUrl: "/",
+    previewUrl: "https://jadidkhata.com/",
+  },
+  {
+    id: 11,
+    title: "Ayaan Health Care",
+    description: "Online Hospital Management System",
+    image: assets.ahc.src,
+    tag: ["All", "Laraval"],
+    gitUrl: "/",
+    previewUrl: "https://www.ayaanhealthcenter.com/",
+  },
+  {
+    id: 12,
+    title: "Burger Point",
+    description: "Point of Sale System",
+    image: assets.BurgerStation.src,
+    tag: ["All", "Laraval"],
+    gitUrl: "/",
+    previewUrl: "http://burgetstation.pk/",
+  },
+  {
+    id: 13,
+    title: "Shoab Kanwal Academy",
+    description: "Online learning management system",
+    image: assets.ska.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "http://shoaibkanwalacademy.com/",
+  },
+  {
+    id: 17,
+    title: "Sabzi Express",
+    description: "Online Grocery delivery service",
+    image: assets.sabziexpress.src,
+    tag: ["All", "Laravel"],
+    gitUrl: "/",
+    previewUrl: "http://sabziexpress.com/",
+  },
+  {
+    id: 19,
+    title: "Aptive Fitness App",
+    description: "Online Fitness App",
+    image: assets.aaptive.src,
+    tag: ["All", "Node js"],
+    gitUrl: "/",
+    previewUrl: "https://aaptiv.com/",
+  },
+  {
+    id: 20,
+    title: "Car glow detaling",
+    description: "Online car detailing system",
+    image: assets.carGlow.src,
+    tag: ["All", "Node js"],
+    gitUrl: "/",
+    previewUrl: "https://carglow.com.pk/",
+  },
+  {
+    id: 21,
+    title: "Aam Click",
+    description: "Online earning platform, where you can earn money through investment with one click",
+    image: assets.AamClick.src,
+    tag: ["All", "Python"],
+    gitUrl: "/",
+    previewUrl: "https://aamclick.com/",
+  },
+  {
+    id: 22,
+    title: "The Black Academy",
+    description: "",
+    image: assets.BlackAcademy.src,
+    tag: ["All", "WordPress"],
+    gitUrl: "/",
+    previewUrl: "https://theblackacademy.com/",
+  },
+  {
+    id: 23,
+    title: "Laa Fee des Boites",
+    description: "",
+    image: assets.desboites.src,
+    tag: ["All", "WordPress"],
+    gitUrl: "/",
+    previewUrl: "https://lafeedesboites.fr/",
   },
 ];
 
@@ -94,13 +283,28 @@ const ProjectsSection = () => {
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
+          name="Laravel"
+          isSelected={tag === "Laravel"}
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Mobile"
-          isSelected={tag === "Mobile"}
+          name="PHP"
+          isSelected={tag === "PHP"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Node js"
+          isSelected={tag === "Node js"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Python"
+          isSelected={tag === "Python"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="WordPress"
+          isSelected={tag === "WordPress"}
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
